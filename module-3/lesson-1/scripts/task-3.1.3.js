14f24aeeded2498b07fc8708a44053c6941bf80a
rubyspch <ruby_chohan@hotmail.co.uk>
2021-01-25 11:49:46 +0100

    Update task-3.1.3.js
    Add variable to javascript

---
+++ module-3/lesson-1/scripts/task-3.1.3.js
@@ -0,0 +1,7 @@
+var NumberOfKittens= "5"; // This defines the variable
+
+alert(NumberOfKittens); // Creates a pop up when the page loads that displays the number we defined
+
+console.log(NumberOfKittens); // Creates a message in the console when a page is inspected showing the number that defined the variable
+
+document.write(NumberOfKittens); // Adds content to the page showing the defined variable value
