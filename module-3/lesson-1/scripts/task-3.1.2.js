98ae3ad8baeee8fe2bcd5dfdcf405cbd5f43c438
rubyspch <ruby_chohan@hotmail.co.uk>
2021-01-25 11:29:49 +0100

    Update task-3.1.2.js
    Add alert, console.log and document.write

---
+++ module-3/lesson-1/scripts/task-3.1.2.js
@@ -0,0 +1,8 @@
+/* This is a comment
+on multiple lines */
+
+alert("Hello alert!"); // Creates a pop up when the page loads
+
+console.log("Hello console!"); // Creates a message in the console when a page is inspected
+
+document.write("Hello document!"); // Adds content to the page
