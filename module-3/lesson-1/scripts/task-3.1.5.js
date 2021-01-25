a00941a3fcade050258e5dd855bdc03b4477e853
rubyspch <ruby_chohan@hotmail.co.uk>
2021-01-25 12:34:26 +0100

    Update task-3.1.5.js
    Add variables of strings and concatonate them

---
+++ module-3/lesson-1/scripts/task-3.1.5.js
@@ -0,0 +1,11 @@
+var firstName= 'Ruby ';
+var fullName= firstName + 'Chohan';
+console.log(fullName); //This outputs the first and second name
+
+var FirstName= 'Ruby ';
+FirstName += 'Spreckley '; //This redefines FirstName to add middle name
+alert(FirstName); //This outputs first and middle name
+
+var FullName= FirstName+'Chohan'; // Adds surname to first and middle names
+document.write(FullName); //This displays all names together
+
