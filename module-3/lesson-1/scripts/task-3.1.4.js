16974ef9590870c96c7b09cc463876aae508beaf
rubyspch <ruby_chohan@hotmail.co.uk>
2021-01-25 12:05:40 +0100

    Update task-3.1.4.js
    Define variables and document.write

---
+++ module-3/lesson-1/scripts/task-3.1.4.js
@@ -0,0 +1,4 @@
+var NumberOfBooks= 7; // define number of books
+var NumberOfDays=60; // define number of days
+var ReadEachBook= NumberOfDays/NumberOfBooks; //Defines number of days to read each book by dividing the values for number of days and number of books
+document.write(ReadEachBook); // displays the value for number of days to read each book
