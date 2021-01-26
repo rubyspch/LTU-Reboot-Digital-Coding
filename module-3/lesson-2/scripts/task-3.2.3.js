8b2efedf3bb137e2b9bf7cee9bb827ed00f49744
rubyspch <ruby_chohan@hotmail.co.uk>
2021-01-26 11:50:57 +0100

    Update task-3.2.3.js
    Add return to function showFullName

--- module-3/lesson-2/scripts/task-3.2.3.js
+++ module-3/lesson-2/scripts/task-3.2.3.js
@@ -10,5 +10,12 @@ var message = greet('Hello', 'Luke');
 /* EXAMPLE */
 
 // ADD YOUR CODE BELOW HERE
+function showFullName(fName, sName){ 
+  var fullName = fName + sName + '<br>';
+  return fullName; 
+}
 
+var person1 = showFullName('Ruby ', 'Chohan'); //This stores the function Ruby+Chohan+br into person1 variable, then you can display it later
+
+document.write(person1); //This displays the function output that has been stored into person1 variable
 // ADD YOUR CODE ABOVE HERE
