93f90edc6d2f46e7d1d8c9d742cffbbffebdf4dc
rubyspch <ruby_chohan@hotmail.co.uk>
2021-01-26 12:29:23 +0100

    Update task-3.2.4.js
    Add else instructions for variable tempurature

--- module-3/lesson-2/scripts/task-3.2.4.js
+++ module-3/lesson-2/scripts/task-3.2.4.js
@@ -1,7 +1,7 @@
-var tempurature= 80 
+var tempurature= 49
     if (tempurature<50){ //Condition for executing the function
         document.write('You need a coat'); //Output if temp is less that 50
     }
-    if (tempurature>=50){  //Condition for executing the function
+    else {
         document.write('You do not need a coat'); //Output is temp is more than or equal to 50
     }
