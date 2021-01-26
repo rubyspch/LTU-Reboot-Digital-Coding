a678245ae5eb17277fb6e2ae91cdda176c4a0d05
rubyspch <ruby_chohan@hotmail.co.uk>
2021-01-26 11:33:31 +0100

    Update task-3.2.2.js
    Add function showFullName

---
+++ module-3/lesson-2/scripts/task-3.2.2.js
@@ -0,0 +1,6 @@
+function showFullName(fName, sName){ //This is the declaration
+    var fullName = fName + sName //The variable defined using the placeholder arguments
+    document.write(fullName + '<br>'); //Output the full name
+}
+showFullName('Ruby ','Chohan'); // This is the arguments to be applied to the function
+showFullName('Matthew ', 'Williams');
