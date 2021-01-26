76d1ee66d27ff06341c2cc4309300a76154a8af0
rubyspch <ruby_chohan@hotmail.co.uk>
2021-01-26 11:01:03 +0100

    Update task-3.2.1.js
    Add notes to function

--- module-3/lesson-2/scripts/task-3.2.1.js
+++ module-3/lesson-2/scripts/task-3.2.1.js
@@ -1,5 +1,5 @@
-function myName(){
-    console.log('My name is Ruby');
-}
+function myName(){ // This names the function 
+    console.log('My name is Ruby'); // This is what the function does 
+} 
 
-myName();
+myName(); // This invokes the function to run it
