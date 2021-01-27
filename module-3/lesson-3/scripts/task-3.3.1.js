432d54cc4912488c36847f78429375409a768858
rubyspch <ruby_chohan@hotmail.co.uk>
2021-01-27 11:12:08 +0100

    Update task-3.3.1.js
    Add function percentageCalculator

---
+++ module-3/lesson-3/scripts/task-3.3.1.js
@@ -0,0 +1,12 @@
+function percentageCalculator(_number, _percentage){
+ let _result= (_number*_percentage) / 100;
+ return _result;
+}
+
+console.log(percentageCalculator(135, 4));
+
+/* OR
+let _number= 135;
+let _percentage=4;
+
+console.log(percentageCalculator(_number, _percentage)); */
