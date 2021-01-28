45e390bff34213a5a4dba4f965b32ceeeee2ab48
rubyspch <ruby_chohan@hotmail.co.uk>
2021-01-28 11:50:08 +0100

    Create task-3.4.3.js
    Add favouriteFoods array and for loop

---
+++ module-3/lesson-4/scripts/task-3.4.3.js
@@ -0,0 +1,7 @@
+var favouriteFoods=['Lasagne','Crisps', 'Aglio e olio', 'Hummus'];
+
+document.write(favouriteFoods[0]);
+
+for (index=0; index<favouriteFoods.length; index++){
+    document.write(`${favouriteFoods[index]} <br>`);
+}
