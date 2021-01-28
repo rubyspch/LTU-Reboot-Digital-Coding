dd7b4a0f7617f992360d70c4b1d48c310f223258
rubyspch <ruby_chohan@hotmail.co.uk>
2021-01-28 11:15:00 +0100

    Create task-3.4.1.js
    Add for loop for 7 times table

---
+++ module-3/lesson-4/scripts/task-3.4.1.js
@@ -0,0 +1,9 @@
+/*for (var num=1; num<100; num++){
+    if (num%7===0){
+        console.log(num);
+    } 
+}*/
+console.log(`This is the 7 times table`); //Find a way to include this inside the loop, and include the other times tables
+for (var num=1; num<=12; num++){
+    console.log(`${num} x 7 = ${num*7}`); //'7' + ' X ' + num +' = ' + num*7 
+}
