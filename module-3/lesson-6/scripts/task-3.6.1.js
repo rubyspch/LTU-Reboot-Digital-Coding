832f190bda13c8e9c597359926cfacd3529f7159
rubyspch <ruby_chohan@hotmail.co.uk>
2021-02-02 11:54:23 +0100

    Create task-3.6.1.js
    Select and change img attribute 'src.
    Select and style tag 'p' [1]

---
+++ module-3/lesson-6/scripts/task-3.6.1.js
@@ -0,0 +1,6 @@
+let mainImage= document.querySelector("img");
+    mainImage.setAttribute('src', 'https://www.innovationvisual.com/hs-fs/hubfs/ecosia-logo.png?width=300&name=ecosia-logo.png'); // If you leave the second quotes blank the image is removed and you'll see alt text, effectively it gets rid of the link to the image
+
+let secondParagraph= document.querySelectorAll('p')[1];
+    secondParagraph.style.border='4px solid orange';
+    secondParagraph.style.padding='40px';
